12d32d550a30d62855a284711bbb473ec02bd702
carymono <thanawit.g@ku.th>
2017-04-03 00:54:06 +0700

    added loadout scene, adjust some item, link loadout scene and the next scene

---
+++ Assets/Scripts/UserPreference.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UserPreference : MonoBehaviour {
+	
+	public static string preferedGun;
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("Current Preference Gun : " + preferedGun);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		handleClicking ();
+	}
+	void handleClicking(){
+		if(Input.GetMouseButtonDown(0)){
+			RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+			if (hit) {
+				if ( hit.collider.gameObject == GameObject.Find("Handgun")){
+					Debug.Log ("Handgun Clicked");
+					preferedGun = "Handgun";
+					SceneManager.LoadScene (2);
+
+				}  
+				else if(hit.collider.gameObject == GameObject.Find("Shotgun")) {
+					Debug.Log ("Shotgun Clicked");
+					preferedGun = "Shotgun";
+					SceneManager.LoadScene (2);
+				}
+				else if(hit.collider.gameObject == GameObject.Find("Rifle")) {
+					Debug.Log ("Rifle Clicked");
+					preferedGun = "Rifle";
+					SceneManager.LoadScene (2);
+				}
+			}
+
+		}
+	}
+}
