543176f99b311f44a9212016de9a90da5bcff30a
carymono <thanawit.g@ku.th>
2017-04-04 11:46:27 +0700

    finish gun rotation, change a lot of pivot point, try creating a location

---
+++ Assets/GameController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+	public int numberPlayer;
+	public GameObject player;
+	public Dictionary<string, int> allPlayer;
+
+
+	// Use this for initialization
+	void Start () {
+		//initialize dictionary
+		allPlayer = new Dictionary<string,int> ();
+
+		//added all player into dictionaary
+		for(int i =0 ; i < numberPlayer; i++){
+			allPlayer.Add ("ID Number "+i, i);
+		}
+
+		//create all player based on current dictionary
+		for(int i =0 ; i < allPlayer.Count ; i++){
+			Vector2 playerPos = new Vector2 (5+(i*2) , 8+(i*2)   );
+
+
+
+			Instantiate (player,playerPos,Quaternion.identity);
+
+
+		}
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Weapon.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector2 mouseScreenPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition) ;
+		//Vector2 direction = mouseScreenPosition - (Vector2)transform.position.normalized;
+		//transform.up = new Vector2(direction.x , direction.y);
+		//transform.LookAt(mouseScreenPosition);
+
+		Vector3 dir = Input.mousePosition - Camera.main.WorldToScreenPoint (transform.position);
+		float angle = Mathf.Atan2(dir.y,dir.x) * Mathf.Rad2Deg;
+		transform.rotation = Quaternion.AngleAxis (angle,Vector3.forward);
+		Flip ();
+
+
+	}
+	void Flip(){
+		if (Input.GetAxis("Horizontal") < -0.1f)
+		{
+			this.GetComponent<SpriteRenderer> ().flipY = true;
+			transform.localScale = new Vector3(-1, 1, 1);
+		}
+		if (Input.GetAxis("Horizontal") > 0.1f)
+		{
+			this.GetComponent<SpriteRenderer> ().flipY = false;
+			transform.localScale = new Vector3(1, 1, 1);
+		}
+
+	}
+}
