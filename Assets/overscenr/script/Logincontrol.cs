3a97dec202008e2811cf58a72be1de1873e73c72
poom <poom10510@gmail.com>
2017-04-09 19:18:45 +0700

    add logo

--- Assets/overscenr/script/Logincontrol.cs
+++ Assets/overscenr/script/Logincontrol.cs
@@ -7,8 +7,9 @@ public class Logincontrol : MonoBehaviour {
 
     private Vector2[] move = new Vector2[3];
     private int npoint = 0;
-	// Use this for initialization
-	void Start () {
+    private bool shoot = true;
+    // Use this for initialization
+    void Start () {
         move[0] = new Vector2(-6.5f, 0.18f);
         move[1] = new Vector2(-6.5f, -1.88f);
         move[2] = new Vector2(-6.5f, -3.86f);
@@ -17,14 +18,18 @@ public class Logincontrol : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (ControlFreak2.CF2Input.GetKeyUp(KeyCode.Space)){
-            Instantiate(bullet, player.transform.position, Quaternion.identity);
+		if (ControlFreak2.CF2Input.GetKeyUp(KeyCode.Space)&&shoot){
+            Vector2 posi = player.transform.position;
+            posi.x += 2f;
+            Instantiate(bullet, posi, Quaternion.identity);
         }
         if (ControlFreak2.CF2Input.GetKeyUp(KeyCode.LeftArrow)) {
+            shoot = false;
             player.transform.localRotation = Quaternion.Euler(0, 180, 0);
         }
         if (ControlFreak2.CF2Input.GetKeyUp(KeyCode.RightArrow))
         {
+            shoot = true;
             player.transform.localRotation = Quaternion.Euler(0, 0, 0);
         }
         if (ControlFreak2.CF2Input.GetKeyUp(KeyCode.UpArrow))
